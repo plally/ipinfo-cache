2f4b01aeb74a339dcc1da85b31c7aeb90e4d823d
Pierce <pierce@vulpes.dev>
2022-07-24 04:10:28 -0400

    formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ipinfo-cache
+
 simple proof of concept cache for ipinfo api
--- src/index.ts
+++ src/index.ts
@@ -9,36 +9,40 @@
  */
 
 export interface Env {
-  IP_STORE: KVNamespace;
+	IP_STORE: KVNamespace;
 }
 
-import { Hono } from 'hono'
-const app = new Hono()
- 
+import { Hono } from 'hono';
+const app = new Hono();
+
 app.get('/ipinfo/:ip', async (c) => {
-  const rawIP = c.req.param('ip')
-  const ip = decodeURI(rawIP)
+	const rawIP = c.req.param('ip');
+	const ip = decodeURI(rawIP);
 
-  const cachedData = await c.env.IP_STORE.get(ip, {type:"json"})
-  if(cachedData?.ip == ip) {
-    return c.json(cachedData)
-  }
+	const cachedData = await c.env.IP_STORE.get(ip, { type: 'json' });
+	if (cachedData?.ip == ip) {
+		return c.json(cachedData);
+	}
 
-  const token = c.req.query('token')
+	const token = c.req.query('token');
 
-  const resp = await fetch(`https://ipinfo.io/${ip}?token=${token}`)
-  if(resp.status != 200){
-    return resp
-  }
+	const resp = await fetch(`https://ipinfo.io/${ip}?token=${token}`);
+	if (resp.status != 200) {
+		return resp;
+	}
 
-  const data: any = await resp.json()
-    
-  await c.env.IP_STORE.put(ip, JSON.stringify({
-    ...data,
-    cacheUpdatedAt: Date.now()
-  }), {expirationTtl: 86400 })
+	const data: any = await resp.json();
 
-  return c.json(data)
-})
- 
- export default app
+	await c.env.IP_STORE.put(
+		ip,
+		JSON.stringify({
+			...data,
+			cacheUpdatedAt: Date.now()
+		}),
+		{ expirationTtl: 43200 }
+	);
+
+	return c.json(data);
+});
+
+export default app;
